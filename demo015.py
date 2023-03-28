a3261017cad72933d12bcfe6b6c5595091d5bc31
yang20002020 <2628636987@qq.com>
2023-03-28 08:09:55 +0800

    遍历列表

---
+++ demo015.py
@@ -0,0 +1,13 @@
+"""
+遍历列表
+"""
+
+a = [3, 7, 4, 2, 6]
+alen = len(a)
+for i in range(alen):
+    print(a[i])
+for i in range(alen):
+    print(a[i], end=',')
+print("***************")
+for e in a:
+    print(e)
