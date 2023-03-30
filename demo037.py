9ef346379132ce3f07e58b7f041e0df0d1cae373
yang20002020 <2628636987@qq.com>
2023-03-30 21:41:36 +0800

    定义类

---
+++ demo037.py
@@ -0,0 +1,52 @@
+import time
+
+
+class DigitClock:
+    """
+    数字时钟  定义类
+    """
+
+    def __init__(self):
+        """
+        初始化方法
+        """
+        cur_time = time.localtime()
+        self._hour = cur_time.tm_hour
+        self._minute = cur_time.tm_hour
+        self._second = cur_time.tm_sec
+
+    def run(self):
+        """
+        时钟行走方法
+
+        """
+        self._second += 1
+        if self._second == 60:
+            self._second = 0
+            self._minute += 1
+
+        if self._minute == 60:
+            self._minute = 0
+            self._hour += 1
+        if self._hour == 24:
+            self._hour = 0
+
+    def show(self):
+        """
+        显示时间
+
+        """
+        return f"{self._hour}:{self._minute}:{self._second}"
+
+
+def main():
+    myclock = DigitClock()
+    # 走10步 自动停止
+    steps = 10
+    for _ in range(steps):
+        print(myclock.show())
+        time.sleep(1)
+        myclock.run()
+
+
+main()
