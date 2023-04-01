63c9f80299e3091f2820f572c2aa27809ebdb107
yang20002020 <2628636987@qq.com>
2023-04-01 10:05:05 +0800

    time 模块

--- demo049.py
+++ demo049.py
@@ -9,4 +9,7 @@ a = time.strftime("%Y-%m-%d %H:%M:%S", now)
 print(a)  # 2023-04-01 09:52:28
 a = time.time()
 b = time.ctime(a)
-print(b) # Sat Apr  1 09:54:46 2023
+print(b)  # Sat Apr  1 09:54:46 2023
+
+c = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+print(c) # 2023-04-01 10:03:46
